8d0bab1a4848029eb948feb83c6e7d996ad6d137
Lucas Basaglia <lcamanducci@gmail.com>
2021-03-30 12:50:06 -0300

    finalizado alteracao, ta faltando tal coisa estou duvida

---
+++ exercicios_sec5/exercicio36.py
@@ -0,0 +1,13 @@
+'''
+    Observação: Preciso fazer tal coisa e estou com duvida na seguinte situação. Posso proceder como?
+'''
+notas = [3,5,6,8,9]
+
+x = 0
+soma = 0
+
+while x < 5:
+    soma=soma+notas[x]
+    x = x + 1
+
+print(soma/5)
---
+++ exercicios_sec5/exercicio37.py
@@ -0,0 +1,12 @@
+valores = [0,0,0,0,0]
+
+print(valores)
+
+x = 0
+
+while x < 5:
+    numero = int(input('Digite um número: '))
+    valores[x] = numero
+    x = x + 1
+
+print(valores)
